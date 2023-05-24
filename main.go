b71d23d15755970c8aafd016e39ef0c30ee48ed1
ckevin <kevin.chaffaux@ynov.com>
2023-05-24 17:50:49 +0200

    :lipstick: | working on login and nav style

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ var port = ":8080"
 func main() {
 	//handlers
 	http.HandleFunc("/", handler.MainHandler)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/fonts"))))
 	fmt.Println("(http://localhost"+port+"/home"+") - Server started on port", port)
 	http.ListenAndServe(port, nil)
 }
