be47de4496ba47463cdaafcf703632996c3988d5
ckevin <kevin.chaffaux@ynov.com>
2023-05-30 16:36:12 +0200

    :sparkles: | get data from form

--- pkg/handler/account.go
+++ pkg/handler/account.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
@@ -14,5 +15,15 @@ type AccountPage struct {
 func Account(w http.ResponseWriter, r *http.Request) {
 	p := AccountPage{Username: "XxDarkSasukexX", Email: "XxDarkSasukexX@gmail.com", Profile_picture: "../img/PP.png"}
 	t := template.Must(template.ParseFiles("./static/html/account.html", "./static/html/navBar.html"))
+	delAccount := r.FormValue("delAccount")
+	editMail := r.FormValue("editMail")
+	changedPwd := r.FormValue("changedPwd")
+	if delAccount != "" {
+		fmt.Println(delAccount)
+	} else if editMail != "" {
+		fmt.Println(editMail)
+	} else if changedPwd != "" {
+		fmt.Println(changedPwd)
+	}
 	t.ExecuteTemplate(w, "account", p)
 }
