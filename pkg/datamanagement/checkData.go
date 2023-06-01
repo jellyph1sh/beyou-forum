bfd2690f9d951180ab5623bf1d14d1dc603afe1d
lucas.bigot@ynov.com <lucas.bigot@ynov.com>
2023-06-01 16:49:27 +0200

    :rocket: | start register

--- pkg/datamanagement/checkData.go
+++ pkg/datamanagement/checkData.go
@@ -10,39 +10,64 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func IsUserExist(userInput string, password string) bool {
-	passwordByte := []byte(password)
-	passwordInSha256 := sha256.Sum256(passwordByte)
-	stringPasswordInSha256 := fmt.Sprintf("%x", passwordInSha256[:])
+func IsUserExist(userEmail string, userProfilName string) bool {
 	db, err := sql.Open("sqlite3", "./DB-Forum.db")
 	if err != nil {
 		fmt.Println("Could not open database : \n", err)
 		return false
 	}
 	defer db.Close()
-	row, err := db.Query("SELECT * FROM User;")
+	QUERY := "SELECT * FROM User WHERE ( user_name = '" + string(userProfilName) + "' OR email = '" + string(userEmail) + "');"
+	row, err := db.Query(QUERY)
 	if err != nil {
 		fmt.Println("Invalid request :")
 		log.Fatal(err)
 		return false
 	}
 	defer row.Close()
-	for row.Next() { // Iterate and fetch the records from result cursor
-		var id int
-		var name string
-		var first_name string
-		var user_name string
-		var email string
-		var password string
-		var is_admin bool
-		var is_valid bool
-		var description string
-		var profile_image string
-		var creation_date time.Time
-		row.Scan(&id, &name, &first_name, &user_name, &email, &password, &is_admin, &is_valid, &description, &profile_image, &creation_date)
-		if (name == userInput || email == userInput) && password == stringPasswordInSha256 {
-			return true
+	if row.Next() == false {
+		return false
+	} else {
+		return true
+	}
+}
+
+func IsRegister(userInput string, password string) (bool, int) {
+	passwordByte := []byte(password)
+	passwordInSha256 := sha256.Sum256(passwordByte)
+	stringPasswordInSha256 := fmt.Sprintf("%x", passwordInSha256[:])
+	db, err := sql.Open("sqlite3", "./DB-Forum.db")
+	if err != nil {
+		fmt.Println("Could not open database : \n", err)
+		return false, 1
+	}
+	defer db.Close()
+	QUERY := "SELECT * FROM User WHERE ( user_name = '" + string(userInput) + "' OR email = '" + string(userInput) + "' ) AND password = '" + string(stringPasswordInSha256) + "';"
+	row, err := db.Query(QUERY)
+	if err != nil {
+		fmt.Println("Invalid request :")
+		log.Fatal(err)
+		return false, 1
+	}
+	defer row.Close()
+	if row.Next() == false {
+		return false, 1
+	} else {
+		for row.Next() {
+			var id int
+			var name string
+			var first_name string
+			var user_name string
+			var email string
+			var password string
+			var is_admin bool
+			var is_valid bool
+			var description string
+			var profile_image string
+			var creation_date time.Time
+			row.Scan(&id, &name, &first_name, &user_name, &email, &password, &is_admin, &is_valid, &description, &profile_image, &creation_date)
+			return true, id
 		}
 	}
-	return false
+	return false, 1
 }
--- pkg/handler/register.go
+++ pkg/handler/register.go
@@ -1,11 +1,55 @@
 package handler
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"forum/pkg/datamanagement"
 	"net/http"
 	"text/template"
+	"time"
 )
+
+type register struct {
+	isValid bool
+}
+
+func CreateUser(ID int, userName string, userFirstName string, userMainName string, userEmail string, stringPasswordInSha256 string) datamanagement.User {
+	nUser := datamanagement.User{}
+	nUser.ID = ID
+	nUser.Name = userName
+	nUser.First_name = userFirstName
+	nUser.User_name = userMainName
+	nUser.Email = userEmail
+	nUser.Password = stringPasswordInSha256
+	nUser.Is_admin = false
+	nUser.Is_valid = true
+	nUser.Description = ""
+	nUser.Profile_image = ""
+	nUser.Creation_date = time.Now()
+	return nUser
+}
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./static/html/register.html", "./static/html/navBar.html"))
+	userEmail := r.FormValue("email")
+	userName := r.FormValue("username")
+	userPassword := r.FormValue("password")
+	registerDisplay := register{}
+	registerDisplay.isValid = true
+
+	if !datamanagement.IsUserExist(userEmail, userName) {
+		passwordByte := []byte(userPassword)
+		passwordInSha256 := sha256.Sum256(passwordByte)
+		stringPasswordInSha256 := fmt.Sprintf("%x", passwordInSha256[:])
+		nUser := CreateUser(11, userName, userName, userName, userEmail, stringPasswordInSha256)
+		nDataContainer := datamanagement.DataContainer{}
+		nDataContainer.User = nUser
+		// QUERY := "INSERT INTO  User VALUES (7,'" + userName + "','" + userName + "','" + userName + "','" + userEmail + "','" + stringPasswordInSha256 + "'," + "false, true,'','','" + time.Now().String() + "');"
+		datamanagement.AddLineIntoTargetTable(nDataContainer, "User", 11)
+	} else {
+		registerDisplay.isValid = false
+		fmt.Println("nofvjnorlsfn")
+	}
+	// fmt.Println(userEmail, userName, userPassword)
 	t.ExecuteTemplate(w, "register", nil)
 }
