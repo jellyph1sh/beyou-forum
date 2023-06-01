9fb378a6689b93aff6a17a92e3dde4f90411bba4
fevan <evan.ferron53@gmail.com>
2023-06-01 14:43:24 +0200

    :rocket: create UpdateLike func, status: is working

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var port = ":8080"
 
 func main() {
-	// topicTest := datamanagement.DataContainer{Topic: datamanagement.Topic{ID: 1, Title: "topic test", Description: "le topic test", Is_valid: true, Follow: []int{1}, Creator: 1}}
+	topicTest := datamanagement.DataContainer{Topic: datamanagement.Topic{ID: 1, Title: "topic test", Description: "le topic test", Is_valid: true, Follow: []int{1}, Creator: 1, Like: 0}}
 	// postTest := datamanagement.DataContainer{Post: datamanagement.Post{ID: 1, Like: 15, Author_id: 1, Is_valid: true, Content: "Post test", Comentary: []int{}, Dislike: 2, Topic: 1}}
 	// postTest2 := datamanagement.DataContainer{Post: datamanagement.Post{ID: 2, Like: 30, Author_id: 1, Is_valid: true, Content: "Post test2", Comentary: []int{}, Dislike: 100, Topic: 1}}
 	// postTest3 := datamanagement.DataContainer{Post: datamanagement.Post{ID: 3, Like: 10000, Author_id: 1, Is_valid: true, Content: "Post test3", Comentary: []int{}, Dislike: 2, Topic: 1}}
@@ -19,12 +19,14 @@ func main() {
 	// datamanagement.AddLineIntoTargetTable(postTest, "Post", 9)
 	// datamanagement.AddLineIntoTargetTable(postTest2, "Post", 9)
 	// datamanagement.AddLineIntoTargetTable(postTest3, "Post", 9)
+	datamanagement.AddLineIntoTargetTable(topicTest, "Topic", 7)
 	// fmt.Println(datamanagement.GetDataForOnePost(1))
 	// profileTest := datamanagement.GetProfileData(1)
 	// fmt.Println(profileTest.User_name, profileTest.Email, profileTest.Profile_image)
 	// sortTopic := datamanagement.GetSortTopic()
 	// fmt.Println(sortTopic[0].Like, sortTopic[1].Like, sortTopic[2].Like)4
-	fmt.Println(datamanagement.GetSortPost())
+	// fmt.Println(datamanagement.GetSortPost())
+	datamanagement.UpdateLike("Topic", topicTest, true, 1)
 	//handlers
 	http.HandleFunc("/", handler.MainHandler)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
