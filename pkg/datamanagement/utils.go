3723102d40fdb24ca73bf417ebff06ea611c9c4a
ckevin <kevin.chaffaux@ynov.com>
2023-06-14 22:44:07 +0200

    :lipstick: | finish desktop profile page / :art: | optimized font imports

--- pkg/datamanagement/utils.go
+++ pkg/datamanagement/utils.go
@@ -71,14 +71,15 @@ type UserConnected struct {
 }
 
 type Topics struct {
-	TopicID     int
-	Title       string
-	Description string
-	Picture     string
-	CreatorID   int
-	Upvotes     int
-	Follows     int
-	ValidTopic  bool
+	TopicID      int
+	Title        string
+	Description  string
+	Picture      string
+	CreatorID    string
+	CreationDate time.Time
+	Upvotes      int
+	Follows      int
+	ValidTopic   bool
 }
 
 type TopicsTags struct {
