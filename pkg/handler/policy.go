88a209f9e8228c226c7611bb96f484f59fc5fba0
ckevin <kevin.chaffaux@ynov.com>
2023-06-21 21:30:03 +0200

    :bug: | fix cookies policy datas send

--- pkg/handler/policy.go
+++ pkg/handler/policy.go
@@ -1,13 +1,23 @@
 package handler
 
 import (
+	"forum/pkg/datamanagement"
 	"net/http"
 	"text/template"
 )
 
+type informationConnection struct {
+	IsAdmin     bool
+	IsConnected string
+}
+
 func Policy(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./static/html/policy.html", "./static/html/navBar.html"))
+	dataPolicy := informationConnection{}
 	cookieConnected, _ := r.Cookie("isConnected")
 	IsConnected := getCookieValue(cookieConnected)
-	t.ExecuteTemplate(w, "policy", IsConnected)
+	dataPolicy.IsConnected = IsConnected
+	cookieIdUser, _ := r.Cookie("idUser")
+	dataPolicy.IsAdmin = datamanagement.IsAdmin(getCookieValue(cookieIdUser))
+	t.ExecuteTemplate(w, "policy", dataPolicy)
 }
